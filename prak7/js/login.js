89385e628d2c150ca3655e327869b331f785b382
MaximLucifer <55010580+MaximLucifer@users.noreply.github.com>
2023-11-20 21:08:56 +0300

    Delete prak7 directory

--- prak7/js/login.js
+++
@@ -1,7 +0,0 @@
-async function get_token() {
-    fetch('http://127.0.0.1:5000/login', {
-        method: 'post'
-    }).then(response => response.text()).then(data => {
-        alert(data);
-    })
-}
