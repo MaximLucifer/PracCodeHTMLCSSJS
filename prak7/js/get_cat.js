96b8dfd19b13c4ab6b5195609393d2203edec0b9
MaximLucifer <slenderman132456@gmail.com>
2023-12-13 02:38:40 +0300

    Add files via upload

--- prak7/js/get_cat.js
+++ prak7/js/get_cat.js
@@ -1,9 +1,10 @@
 async function get_cat() {
-    formData.append('token', 'тут типа лежит твой уникальный, персональный токен (со всей любовью)')
-    formData.append('code', '0')
+    let data = new FormData(get_cat_form)
+    data.append('token', 'тут типа лежит твой уникальный, персональный токен (со всей любовью)')
+    // formData.append('code', '0')
     fetch('http://127.0.0.1:5000/get_cat', {
         method: 'post',
-        body: formData
+        body: data
     }).then(response => response.blob()).then(data => {
         const imageUrl = URL.createObjectURL(data);
         const imageElement = document.createElement("img");
